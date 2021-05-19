b188e44d9f0e750dae5591f288d64db26d917d16
Nimaniad <adri.glez.98@gmail.com>
2021-05-19 15:33:35 +0100

    Fase final del proyecto

--- src/index.js
+++ src/index.js
@@ -51,7 +51,7 @@ app.use((req, res, next) => {
 // Rutas
 app.use(require('./routes'));
 app.use(require('./routes/authentication'));
-app.use('/links', require('./routes/links'));
+//app.use('/links', require('./routes/links'));
 
 // Archivos Públicos
 app.use(express.static(path.join(__dirname, 'public')));
